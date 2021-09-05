0a8fc6ea59a7ce298f582bd3d7f6373d7ff20972
Andrii Moisol <andrii.moisol@gmail.com>
2021-09-05 19:50:54 +0300

    Initial setup

--- PacMan/ViewController.swift
+++ PacMan/ViewController.swift
@@ -17,14 +17,9 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
